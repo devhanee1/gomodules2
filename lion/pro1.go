bcc6b42734e99e04e393990a86e38041612577c5
Kyounghan Park <kyopark@ea.com>
2024-03-11 14:08:07 -0700

    first commit

---
+++ lion/pro1.go
@@ -0,0 +1,7 @@
+package lion
+
+import "fmt"
+
+func LionA(){
+	fmt.Println("Module gomodules2, package lion, function LionA()")
+}
---
+++ tiger/pro1.go
@@ -0,0 +1,7 @@
+package tiger 
+
+import "fmt"
+
+func TigerA(){
+	fmt.Println("Module gomodules2, package tiger, function TigerA()")
+}
